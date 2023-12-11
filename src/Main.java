c35cd47d4a849c51adb36d0f49b19e4b681e37af
DeroPlayz <deroplayz@gmail.com>
2023-12-10 16:12:38 -0800

    modified:   src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import javax.swing.JFrame;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.geom.*;
+
 class Main{
-    public static void main(String[] args) {
+    static JFrame frame = new JFrame("Demo");
     
+    public static void main(String[] args) {
+        frame.setVisible(true);
+        frame.setAutoRequestFocus(true);
+        frame.setSize(1000, 600);
+        Graphics g = frame.getGraphics();
+        g.setColor(Color.RED);
+        g.drawRect(100, 100, 150, 150);
     }
 }
