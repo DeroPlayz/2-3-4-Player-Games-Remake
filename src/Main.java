3472886e02aed02473e2983bdf9b726e4d407a27
DeroPlayz <88916740+DeroPlayz@users.noreply.github.com>
2023-12-12 18:25:39 +0000

    yeah

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,19 @@ import java.awt.Rectangle;
 import java.awt.geom.*;
 
 class Main{
+    public static final Color Red = new Color(255, 0, 0);
+    public static final Color Orange = new Color(247, 120, 5);
+    public static final Color Yellow= new Color(247, 222, 5);
+    public static final Color LimeGreen = new Color(0, 255, 0);
+    public static final Color ForestGreen = new Color(0, 115, 4);
+    
+    public static final Color Cyan = new Color(5, 183, 247);
+    public static final Color DarkBlue = new Color(0, 0, 255);
+    public static final Color Purple = new Color(128, 0, 128);
+    //I like public static final Color Lavender = new Color(128, 0, 255, 128);
+    
+    public static final Color Pink = new Color(247, 5, 188);
+    
     static JFrame frame = new JFrame("2 3 4 Player Games");
     
     public static void main(String[] args) {
