3bde7a40f940db225dfb107c5d5847521a4b74ee
DeroPlayz <88916740+DeroPlayz@users.noreply.github.com>
2023-12-12 17:48:45 +0000

    added colors hehe

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,16 @@ import javax.swing.JFrame;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Rectangle;
 import java.awt.geom.*;
 
 class Main{
-    static JFrame frame = new JFrame("Demo");
+    static JFrame frame = new JFrame("2 3 4 Player Games");
     
     public static void main(String[] args) {
         frame.setVisible(true);
         frame.setAutoRequestFocus(true);
         frame.setSize(1000, 600);
-        Graphics g = frame.getGraphics();
-        g.setColor(Color.RED);
-        g.drawRect(100, 100, 150, 150);
+        
     }
 }
---
+++ src/player/Player.java
@@ -0,0 +1,7 @@
+package player;
+
+public class Player {
+    private int playerNum; public void setPlayerNum(int playerNum){this.playerNum = playerNum;} public int getPlayerNum(){return playerNum;}
+    private Palette
+
+}
